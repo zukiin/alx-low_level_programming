ca24e6ccd84e94dd419e429ba98b25a5bcb65133
zukiin <zukinkalitshana@icloud.com>
2023-03-16 19:13:46 -0700

    Add function to print text using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+/*
+ * File: 5-printf.c
+ * Auth: Zuki Nkalitshana
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints "with proper grammar, but the outcome is a piece of art,"
+ *	And adds new line after.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art");
+	return (0);
+}
+
