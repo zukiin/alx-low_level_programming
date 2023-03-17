353d8ea4e891a960c75df94cb9ea3742fbbcef0b
zukiin <zukinkalitshana@icloud.com>
2023-03-16 18:56:47 -0700

    Add code to print string using alternative to printf() function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+/*
+ * File: 4-puts.c
+ * Author: Zuki Nkalitshana
+ */
+
+#include <stdio.h>
+
+/**
+ * main function prints "Programming is like building a multilingual puzzle"
+ * And adds new line after.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
