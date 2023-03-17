c6d3f489cd40e327c3e48789f7f6c7dc9b7ecba0
zukiin <zukinkalitshana@icloud.com>
2023-03-16 19:59:08 -0700

    Add program to print a string using write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+/*
+ * File: 101-quote.c
+ * Author: Zuki Nkalitshana
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	write("1, and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, 70");
+	return (1);
+}
