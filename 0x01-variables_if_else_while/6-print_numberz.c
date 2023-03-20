30cf9d56465312358b246995f6e4890fa45a66af
zukiin <zukinkalitshana@icloud.com>
2023-03-20 14:43:41 -0700

    Add program to print base10 numbers using ASCII

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+/*
+ * File: 6-print_numberz.c
+ * Author: Zuki Nkalitshana
+ */
+
+#include <stdio.h>
+
+/**
+ * main - print all base 10 numbers using putchar() funct
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n= 0; n < 10; n++)
+	{
+		putchar(n + 48);
+	}
+	putchar('\n');
+	return (0);
+}
+
