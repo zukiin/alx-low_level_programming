ec19c5f339cb620e16d49fcf819ce08869d206e7
zukiin <zukinkalitshana@icloud.com>
2023-03-20 08:09:35 -0700

    Fix condition to evaluate last digit against 0 and remove unnecessary evaluations

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,15 +21,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	last = n % 10;
 
-	if (n > 0 && last > 5)
+	if (last > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, last);
 	}
-	else if (n == 0 && last == 0)
+	else if (last == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, last);
 	}
-	else
+	else if (last < 6)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 	}
