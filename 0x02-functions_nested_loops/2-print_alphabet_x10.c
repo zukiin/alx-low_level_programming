e0f3b75576d1629e93521f522a3edfd6b3865fc7
zukiin <zukinkalitshana@icloud.com>
2023-03-22 06:00:03 -0700

    Fix format of outter loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -16,12 +16,12 @@ void print_alphabet_x10(void)
 	char ch;
 	int n;
 
-	for (n = 0; n <= 10; n++)
+	for (n = 0; n < 10; n++)
 	{
 		for (ch = 'a' ; ch <= 'z'; ch++)
 		{
 			putchar(ch);
 		}
+		putchar('\n');
 	}
-	putchar('\n');
 }
