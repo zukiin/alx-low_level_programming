1e9b8ac682675d7ae6b7dd1e17e97f600c9ed5f1
zukiin <zukinkalitshana@icloud.com>
2023-03-23 20:38:42 -0700

    Add unction that prints the numbers, from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * print_most_numbers - print numbers from 0 to 9
+  *
+  * Return: numbers
+  */
+void print_most_numbers(void)
+{
+	int x = 0;
+
+	for (; x <= 9; x++)
+	{
+		if (x == 2 || x == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(x + '0');
+		}
+	}
+
+	_putchar('\n');
+}
