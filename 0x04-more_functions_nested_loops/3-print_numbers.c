4d3631a72b8d06c715cbd50db63b5493c3f25997
zukiin <zukinkalitshana@icloud.com>
2023-03-23 20:44:24 -0700

    Add function that prints the numbers, from 0 to 9 and renamed '4-print_most_numbers.c'

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,24 +1,18 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
-  * print_most_numbers - print numbers from 0 to 9
-  *
-  * Return: numbers
-  */
-void print_most_numbers(void)
+ * print_numbers - print numbers from 0 to 9
+ *
+ * Return: numbers
+ */
+void print_numbers(void)
 {
-	int x = 0;
+	int x;
 
-	for (; x <= 9; x++)
+	for (x = 0; x <= 9; x++)
 	{
-		if (x == 2 || x == 4)
-		{
-			continue;
-		}
-		else
-		{
-			_putchar(x + '0');
-		}
+		_putchar(x + '0');
 	}
 
 	_putchar('\n');
