9c42aa0aff200e51fdaa4828fa968a14a92685ce
zukiin <zukinkalitshana@icloud.com>
2023-04-04 17:59:49 -0700

    Add funct that prints the sum of the two diagonals of a sq matrix

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagsums - prints the sum of the two diagonals of a square
+ * matrix of int
+ * @a: array name
+ * @size: array size
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i = 0;
+	int j = size - 1;
+	int s, z = 0;
+
+	while (i <= (size * size))
+	{
+		s += a[i];
+		i = i + size + 1;
+	}
+
+	while (j < (size * size - 1))
+	{
+		z += a[j];
+		j = j + size - 1;
+	}
+	printf("%d, %d\n", s, z);
+}
