bdafae0e97a2c60ea8584edcf2748853bef03501
zukiin <zukinkalitshana@icloud.com>
2023-04-12 19:04:07 -0700

    Fix for loop format

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,7 +9,7 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, count;
+	int i, j, count = 0;
 	unsigned int k;
 	char *concatStr = NULL;
 
@@ -25,7 +25,7 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; s1[i] != '\0'; i++)
 	{}
 	for (j = 0; s2[j] != '\0'; j++)
-		;
+	{}
 	concatStr = (char *)malloc((i + j + 1) * sizeof(char));
 	if (concatStr == NULL)
 	{
