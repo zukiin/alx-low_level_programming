13ac069443bb30d9a5e96174db35a05b863bb7fe
zukiin <zukinkalitshana@icloud.com>
2023-04-15 20:07:19 -0700

    Add header file

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifndef main_h
+#define main_h
+
+int _putchar(char c);
+int main(int __attribute__((__unused__)) argc, char *argv[]);
+
+#endif /* main_h */
