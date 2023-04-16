922dcbe3d4a96560f203ff75e6624d095f536b99
zukiin <zukinkalitshana@icloud.com>
2023-04-15 20:08:10 -0700

    Add program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - prints the name of program
+ * @argc: args count
+ * @argv: array of args
+ * Return: 0 for success
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
