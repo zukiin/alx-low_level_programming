7d26f435acf2fc144f9476bfdd4f97a0879b17e1
zukiin <zukinkalitshana@icloud.com>
2023-04-17 20:45:43 -0700

    Add function that prints a struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+
+/**
+ * print_dog - prints struct dog
+ * @d: dog identifier
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	if (d->name != NULL)
+		printf("Name: %s\n", d->name);
+	else
+		printf("(nil)\n");
+
+	if (d->age > 0.0)
+		printf("Age: %.1f\n", d->age);
+	else
+		printf("(nil)\n");
+
+	if (d->owner != NULL)
+		printf("Owner: %s\n", d->owner);
+	else
+		printf("(nil)\n");
+}
---
+++ 0x0E-structures_typedef/test.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    struct dog my_dog;
+
+    my_dog.name = "Milo";
+    my_dog.age = 2.0;
+    my_dog.owner = "Zuki";
+    print_dog(&my_dog);
+    return (0);
+}
