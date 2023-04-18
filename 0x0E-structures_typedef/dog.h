ebedc2231b0c1e7b3434624ecfb56fc706c87d2f
zukiin <zukinkalitshana@icloud.com>
2023-04-17 20:49:54 -0700

    Add definition for new dog type

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,4 +21,9 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
+/**
+*dog_t - new dog type definition
+*/
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
 #endif /* dog_h */
