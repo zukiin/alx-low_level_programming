4806a504a5a4cfa271d5773b835eacaf89411a5b
zukiin <zukinkalitshana@icloud.com>
2023-04-20 20:50:10 -0700

    Refactor code

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,31 +7,31 @@
 void print_all(const char * const format, ...)
 {
 	int a = 0, i;
-	char *separator = ", ", *s;
+	char *separator, *s;
 	va_list li;
 	char c;
 	float f;
 
 	va_start(li, format);
 
-	c = va_arg(li, int);
-	i = va_arg(li, int);
-	f = (float) va_arg(li, double);
-	s = va_arg(li, char *);
-
 	while (format && format[a])
+	{
 		switch (format[a])
 		{
 			case 'c':
+				c = va_arg(li, int);
 				printf("%c", c);
 				break;
 			case 'i':
+				i = va_arg(li, int);
 				printf("%d", i);
 				break;
 			case 'f':
+				f = va_arg(li, double);
 				printf("%f", f);
 				break;
 			case 's':
+				s = va_arg(li, char *);
 				if (s == NULL)
 					printf("(nil)");
 				else
@@ -41,9 +41,12 @@ void print_all(const char * const format, ...)
 				break;
 		}
 	if (format[a + 1])
+	{
+		separator = ", ";
 		printf("%s", separator);
+	}
 	a++;
-
+	}
 	putchar('\n');
 	va_end(li);
 }
