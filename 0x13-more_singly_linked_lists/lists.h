2020d702a12678bc808ad6c5f7091c06e5c48cc4
zukiin <zukinkalitshana@icloud.com>
2023-05-03 18:48:41 -0700

    Fix header file for betty compliance

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -23,7 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index);
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ *
  */
 typedef struct listint_s
 {
