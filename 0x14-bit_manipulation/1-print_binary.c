835ea97e2b4f0940923e13eebb3f27c350fa56bc
zukiin <zukinkalitshana@icloud.com>
2023-05-07 04:35:02 +0200

    Add function that converts a number to base 2

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * print_binary - prints binary number
+  * @n: decimal num to print
+  */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+
+	if (n & 1)
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+}
