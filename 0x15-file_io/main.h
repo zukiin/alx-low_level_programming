acf8506c68369dae866cf4c27d0744f7bf6acfdc
zukiin <zukinkalitshana@icloud.com>
2023-05-09 00:20:04 +0200

    Remove putchar

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,9 +1,11 @@
 #ifndef main_h
 #define main_h
 
-#include <stdlib.>
-#include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
