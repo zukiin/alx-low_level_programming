c61c4c890c1296758ebfd1d31f1f1cf5faafb845
zukinkalitshana <zukinkalitshana@icloud.com>
2023-12-01 03:31:02 +0200

    Add program that displays the information contained in the ELF file

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,127 @@
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+
+/**
+ * check_elf - Checks if a file is an ELF file.
+ * @e_ident: pointer to an arr containing ELF magic numbers.
+ *
+ * Description: If file is not an ELF file, exit with 98.
+ */
+void check_elf(unsigned char *e_ident)
+{
+	int i;
+
+	for (i = 0; i < 4; i++)
+	{
+		if (e_ident[i] != 127 &&
+		    e_ident[i] != 'E' &&
+		    e_ident[i] != 'L' &&
+		    e_ident[i] != 'F')
+		{
+			dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
+			exit(98);
+		}
+	}
+}
+
+/**
+ * print_magic - Prints magic numbers of an ELF header.
+ * @e_ident: pointer to an arr containing ELF magic numbers.
+ *
+ * Description: space-separated magic numbers.
+ */
+void print_magic(unsigned char *e_ident)
+{
+	int i;
+
+	printf(" Magic:   ");
+
+	for (i = 0; i < EI_NIDENT; i++)
+	{
+		printf("%02x", e_ident[i]);
+
+		if (i == EI_NIDENT - 1)
+			printf("\n");
+		else
+			printf(" ");
+	}
+}
+
+/**
+ * print_class - Prints the class of an ELF header.
+ * @e_ident: pointer to an arr containing the ELF class.
+ */
+void print_class(unsigned char *e_ident)
+{
+	printf("  Class:                             ");
+
+	switch (e_ident[EI_CLASS])
+	{
+	case ELFCLASSNONE:
+		printf("none\n");
+		break;
+	case ELFCLASS32:
+		printf("ELF32\n");
+		break;
+	case ELFCLASS64:
+		printf("ELF64\n");
+		break;
+	default:
+		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
+	}
+}
+
+/**
+ * print_data - Prints the data of an ELF header.
+ * @e_ident: pointer to an arr containing the ELF class.
+ */
+void print_data(unsigned char *e_ident)
+{
+	printf("  Data:                           ");
+
+	switch (e_ident[EI_DATA])
+	{
+	case ELFDATANONE:
+		printf("none\n");
+		break;
+	case ELFDATA2LSB:
+		printf("2's compliment, little endian\n");
+		break;
+	case ELFDATA2MSB:
+		printf("2's compliment, big endian\n");
+		break;
+	default:
+		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
+	}
+}
+
+/**
+ * print_version - Prints the version of an ELF header.
+ * @e_ident: pointer to an arr containing the ELF version.
+ */
+void print_version(unsigned char *e_ident)
+{
+	printf(" Version:                        %d",
+	       e_ident[EI_VERSION]);
+
+	switch (e_ident[EI_VERSION])
+	{
+	case EV_CURRENT:
+		printf(" (current)\n");
+		break;
+	default:
+		printf("\n");
+		break;
+	}
+}
