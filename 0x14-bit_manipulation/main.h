92e6c68558455afb93aaf768ced8bd1067f05e0e
zukiin <zukie.tn@gmail.com>
2023-11-30 16:08:22 -0800

    Add global directives

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,9 @@
 #ifndef main_h
 #define main_h
 
+#include <stdio.h>
+#include <stdlib.h>
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int _putchar(char c);
