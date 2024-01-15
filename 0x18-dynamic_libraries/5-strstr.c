b78ec8c694c54e4e6975af245dd7bdb294144048
zukinkalitshana <zukinkalitshana@icloud.com>
2024-01-15 22:16:04 +0200

    Add c files for testing

---
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* _strstr - locates a substring
+* @haystack: text of string to search
+* @needle: string to search for
+*
+* Return: pointer to string result
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int i, ss = 0;
+
+	while (needle[ss] != '\0')
+		ss++;
+
+	while (*haystack)
+	{
+		for (i = 0; needle[i]; i++)
+		{
+			if (haystack[i] != needle[i])
+			{
+				break;
+			}
+		}
+		if (i != ss)
+		{
+			haystack++;
+		}
+		else
+		{
+			return (haystack);
+		}
+	}
+	return (NULL);
+}
